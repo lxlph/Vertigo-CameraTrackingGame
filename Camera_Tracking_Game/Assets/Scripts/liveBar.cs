3b778eb8a8536a0508561aedce8f76afa68a1962
luxil <xuanlinh.do@haw-hamburg.de>
2015-01-10 04:13:07 +0100

    healthbar

---
+++ Camera_Tracking_Game/Assets/Scripts/liveBar.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class liveBar : MonoBehaviour {
+	public Sprite damageImage;
+	public Sprite[]  liveBarSprites = new Sprite[4];
+	public int spriteIndex;
+	private float posY;
+	void Awake () {
+		for (int i = 1; i < liveBarSprites.Length; i++){ 
+			liveBarSprites[i] = Resources.Load<Sprite>("liveBar_" + i.ToString());
+		}
+	}
+
+	void Update () {
+		posY = transform.position.y;
+		Vector3 PlayerPOS = GameObject.Find("Player").transform.transform.position;
+		transform.position = new Vector3(PlayerPOS.x + 15, posY, PlayerPOS.z + 7);
+		//transform.position = new Vector3 (0,0,0); 
+		spriteIndex = GameObject.Find ("Player").GetComponent<PlayerMovement> ().playerHealth;
+		gameObject.GetComponent<SpriteRenderer>().sprite = liveBarSprites[spriteIndex];
+
+		/*
+		if (GameObject.Find ("Player").GetComponent<PlayerController> ().playerHealth==3){
+			gameObject.GetComponent<SpriteRenderer>().sprite = liveBarSprites[3];
+		}
+		*/
+	}
+	
+}
