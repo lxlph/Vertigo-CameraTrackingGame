6a7bacee8326481528dacc94c3c9ca1d66c67114
luxil <xuanlinh.do@haw-hamburg.de>
2015-01-10 01:01:00 +0100

    bisMenu

--- Camera_Tracking_Game/Assets/Menu/Scripts/LoadMenu.cs
+++ Camera_Tracking_Game/Assets/Menu/Scripts/LoadMenu.cs
@@ -6,25 +6,24 @@ public class LoadMenu : MonoBehaviour {
 	void OnGUI(){
 		if (mainMenu){
 			if (GUI.Button(new Rect(280, 130, 120, 45), "Kameramodus")){
-				Time.timeScale = 0;
 				Debug.Log ("Kamera geladen");
 				mainMenu = false;
 				Application.LoadLevel("Camera");
 			}
 			if (GUI.Button(new Rect(280, 180, 120, 45), "Normales Spiel")){
-				Time.timeScale = 0;
 				Debug.Log ("Spiel geladen");
 				mainMenu = false;
 			}
 		}
 		else{
 			if (GUI.Button(new Rect(280, 130, 120, 45), "Level 1")){
-				Time.timeScale = 0;
 				Application.LoadLevel("Level1");
 			}
 			if (GUI.Button(new Rect(280, 180, 120, 45), "Level 2")){
-				Time.timeScale = 0;
 				Application.LoadLevel("Level2");
+			}
+			if (GUI.Button(new Rect(280, 230, 120, 45), "Level 3")){
+				Application.LoadLevel("Level3");
 			}
 		}
 	}
