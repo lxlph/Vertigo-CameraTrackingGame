3d37aa7ac15ce9e4acddffa99ff88fd81ee08acd
Nikolai Reinke <nikolai.reinke@haw-hamburg.de>
2015-01-20 12:01:13 +0100

    EinPaarNeuerungenFuerDiePraesi

---
+++ Camera_Tracking_Game/Assets/buttonsoundsMenu.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonsoundsMenu : MonoBehaviour {
+
+	public AudioClip vertigo;
+	public AudioClip normalMode;
+	public AudioClip cameraMode;
+	public AudioClip buttonSound;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void playNormalMode () {
+		audio.PlayOneShot (normalMode);
+
+	}
+
+	public void playCameraMode () {
+		audio.PlayOneShot (cameraMode);
+		
+	}
+
+	public void vertigoSound () {
+		audio.PlayOneShot (vertigo);
+		
+	}
+
+	public void playButtonSound () {
+		audio.PlayOneShot (buttonSound);
+		
+	}
+}
