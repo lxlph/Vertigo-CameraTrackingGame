13b0c46c2ada696104fe8e1cc2ab357a5f0506e6
Nikolai Reinke <nikolai.reinke@haw-hamburg.de>
2015-01-20 14:44:01 +0100

    NeueVersueVersion

---
+++ Camera_Tracking_Game/Assets/TorchOn.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TorchOn : MonoBehaviour {
+
+	private bool Active;
+	private AndroidJavaObject camera1;
+	
+	void FL_Start()
+	{
+		AndroidJavaClass cameraClass = new AndroidJavaClass("android.hardware.Camera");       
+		WebCamDevice[] devices = WebCamTexture.devices;
+		
+		int camID = 0;
+		camera1 = cameraClass.CallStatic<AndroidJavaObject>("open", camID);
+		
+		if (camera1 != null)
+		{
+			AndroidJavaObject cameraParameters = camera1.Call<AndroidJavaObject>("getParameters");
+			cameraParameters.Call("SET_FLASH_MODE", "torch");
+			camera1.Call("setParameters", cameraParameters);
+			Active = true;
+		}
+		else
+		{
+			Debug.LogError("[CameraParametersAndroid] Camera not available");
+		}
+		
+	}
+	
+	void OnDestroy()
+	{
+		FL_Stop();
+	}
+	
+	void FL_Stop()
+	{
+		
+		if (camera1 != null)
+		{
+			camera1.Call("stopPreview");
+			camera1.Call("release");
+			Active = false;
+		}
+		else
+		{
+			Debug.LogError("[CameraParametersAndroid] Camera not available");
+		}
+		
+	}
+	
+	void OnGUI()
+	{
+		GUILayout.BeginArea(new Rect(Screen.width * 0.1f, Screen.height * 0.1f, Screen.width * 0.3f, Screen.height * 0.1f));
+		if (!Active)
+		{
+			if (GUILayout.Button("ENABLE FLASHLIGHT"))
+				FL_Start();
+		}
+		else
+		{
+			if (GUILayout.Button("DISABLE FLASHLIGHT"))
+				FL_Stop();
+		}
+		GUILayout.EndArea();
+	}
+}
